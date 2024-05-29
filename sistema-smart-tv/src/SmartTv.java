c0d7c1b975d897610fb76089b4aaf6f215ef0ce7
badu51 <emmanuelprata@gmail.com>
2024-05-29 01:24:14 -0300

    novo sistema de tv

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -9,4 +9,21 @@ public class SmartTv {
     public void desligar(){
         ligada = false;
     }
+    public void aumentarVolume(){
+        volume++;
+    }
+    public void diminuirVolume(){
+        volume--;
+    }
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal(){
+        canal++;
+    }
+    public void diminuirCanal(){
+        canal++;
+    }
+        
+
 }

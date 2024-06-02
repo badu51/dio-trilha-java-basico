aa75ba7fdfb382b503fc814e9ecace954059067f
badu51 <emmanuelprata@gmail.com>
2024-06-02 12:03:17 -0300

    update com tratamento de erros

--- excecoes/src/AboutMe.java
+++ excecoes/src/AboutMe.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+import java.util.Locale;
+import java.util.InputMismatchException;
 public class AboutMe {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in).
+        try{ //tratamento de erro
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+        
+        System.out.println("Ola, me chamo "+nome.toUpperCase()+" "+sobrenome);
+        System.out.println("Tenho "+idade+" anos ");
+        System.out.println("Minha altura é "+altura+"cm ");
+        scanner.close();
+        }
+        catch(InputMismatchException e)   { //tipos de entradas nao permitidas acionarão o tratamento de erro
+            System.err.println("Os campos idade e altura precisam ser numéricos");
+        } 
+
+
+
     }
 }
